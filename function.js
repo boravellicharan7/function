2e2991f6a527695869fea3eeb00e84612f3040df
boravellicharan7 <boravellicharan6@gmail.com>
2024-10-06 15:22:58 +0530

    added

---
+++ function.js
@@ -0,0 +1,40 @@
+//Question-1:-
+var a=10;
+var b=20;
+function add(){
+    var c=a+b;
+    console.log(c);
+}
+a=75;
+b=57;
+add()
+
+//Question-2:-
+var a=567;
+var b=8765;
+function sub(){
+    var c=a-b;
+    console.log(c);
+}
+a=75;
+b=57;
+sub()
+
+//Question-3:-
+var a=567;
+var b=8765;
+function prod(){
+    var c=a*b;
+    console.log(c);
+}
+prod()
+
+//Question-4:-
+var a=126;
+var b=0;
+function div(){
+    if(b==0){
+        console.log("Can not divied by zero");   
+    }
+}
+div()
